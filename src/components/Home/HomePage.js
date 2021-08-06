bf1d1b72dbd88c4ec39fb4abaced1973d5c76571
Rustam <rustamotorbaev@gmail.com>
2021-08-06 10:06:48 +0600

    change header

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Container from "../Header/Container";
 import Navbar from "../Header/Navbar";
-import Footer from "./footer";
 
 const HomePage = () => {
   return (
     <React.Fragment>
       <Navbar />
       <Container />
-      <Footer />
     </React.Fragment>
   );
 };
